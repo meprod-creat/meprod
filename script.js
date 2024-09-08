c91b45f1e88b114ddfa288f72be830d3c313861e
meprod-creat <emahalok@gmail.com>
2024-09-08 23:49:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const affParam = '?aff=533';
+    const productLinks = document.querySelectorAll('a[href^="https://crystalquest.com/products/"]');
+
+    productLinks.forEach(link => {
+        let href = link.getAttribute('href');
+        if (href && !href.includes(affParam)) {
+            // Ajouter le paramètre d'affiliation à la fin de l'URL
+            link.setAttribute('href', href + affParam);
+        }
+    });
+});
